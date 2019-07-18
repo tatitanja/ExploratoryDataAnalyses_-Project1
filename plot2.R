ebf02488a204a50319d70be4c3a35450f6e7dd48
tatitanja <46087790+tatitanja@users.noreply.github.com>
2019-07-18 22:16:59 +0200

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,7 @@
+data$datetime <- as.POSIXct(data$datetime)
+attach(data)
+plot(Global_active_power ~ datetime, type = "l",
+     ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.copy(png, file = "plot2.png", height = 480, width = 480)
+dev.off()
+detach(data)
